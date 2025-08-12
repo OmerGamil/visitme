a55edc20cf549f4fa850214f24600532ee0dca4c
Omer Ahmed <omerahmed@Mac.telekom.ip>
2025-08-13 01:52:03 +0200

    Debug

--- visitme/settings.py
+++ visitme/settings.py
@@ -162,3 +162,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"class": "logging.StreamHandler"}},
+    "root": {"handlers": ["console"], "level": "INFO"},
+    "loggers": {
+        "django.request": {"handlers": ["console"], "level": "ERROR", "propagate": False},
+    },
+}
